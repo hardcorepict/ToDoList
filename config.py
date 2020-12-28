d55e75b1e1e5a99a4fc911633eb18acb90a6607f
hardcorepict <58664693+hardcorepict@users.noreply.github.com>
2020-12-28 22:17:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,17 +9,11 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     #Email settings
-    """
+    
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    """
+   
     ADMINS = ['murreyandy89@gmail.com']
-    
-    MAIL_SERVER='smtp.googlemail.com'
-    MAIL_PORT=587
-    MAIL_USE_TLS=1
-    MAIL_USERNAME='murreyandy89@gmail.com'
-    MAIL_PASSWORD='a1coho1e'
